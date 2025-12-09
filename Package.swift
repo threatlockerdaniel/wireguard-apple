a390bbcd9836c2912ffb7bf64950efb6db79c4c0
threatlockerdaniel <daniel.rodriguez@threatlocker.com>
2025-12-09 15:49:40 -0500

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,8 +7,7 @@ let package = Package(
     name: "WireGuardKit",
     platforms: [
         .macOS(.v12),
-        .iOS(.v15),
-	.tvOS(.v17)
+        .iOS(.v15)
     ],
     products: [
         .library(name: "WireGuardKit", targets: ["WireGuardKit"])
